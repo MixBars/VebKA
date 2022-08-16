e30ce96ca6eda69eeacd54e8dc57531e6cf7ec02
MixBars <portalmixbars@gmail.com>
2022-08-16 18:27:57 +0300

    start

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get '/home', to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  #get "/home", to "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
